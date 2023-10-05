599095b241775687774d73f4920d85f5ccfc07a5
T Mark <tmk1.tran@gmail.com>
2023-10-05 11:47:09 -0500

    added style, form functionality, button wired up, components organized

--- src/components/FavoritesList/FavoritesList.jsx
+++ src/components/FavoritesList/FavoritesList.jsx
@@ -2,14 +2,24 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
-export default function Favorites() {
+export default function FavoritesList() {
   return (
-    <Card>
-      <CardContent>
-      <Typography variant="h6" component="div">Title of Image</Typography>
-
-      </CardContent>
-    </Card>
+    <div>
+      <h2>Favorites Page</h2>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" component="div">
+            Title of Image
+          </Typography>
+          Image BODY
+          <br />
+          <br />
+          <br />
+          <br />
+          <Typography variant="captions">Image caption</Typography>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
